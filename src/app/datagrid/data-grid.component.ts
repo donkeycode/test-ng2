1fc74c65a586b95e9d81f7ad6baba41a08a0ee46
Cedric LOMBARDOT <cedric.lombardot@gmail.com>
2017-02-24 21:33:23 +0100

    Use fake rest

--- src/app/datagrid/data-grid.component.ts
+++ src/app/datagrid/data-grid.component.ts
@@ -1,23 +1,37 @@
-import { Component, AfterContentInit, ContentChildren, QueryList, ChangeDetectorRef, Subscription } from '@angular/core';
+import { Component, AfterContentInit, ContentChildren, QueryList, ChangeDetectorRef, OnInit, Input } from '@angular/core';
 import { ColumnComponent } from './column.component';
+import SimpleRestProvider from './data-providers/simple';
+import { GET_LIST } from './data-providers/types';
 
 @Component({
   selector: 'data-grid', 
   templateUrl: './data-grid.component.html'
 })
-export class DataGridComponent implements AfterContentInit {
+export class DataGridComponent implements AfterContentInit, OnInit {
   @ContentChildren(ColumnComponent) private cols: QueryList<ColumnComponent>;
+
+  @Input() private objects: string;
 
   public columns: ColumnComponent[];
 
-  public columnsSubscription: Subscription;
+  public columnsSubscription;
 
-  public rows: Array<any> = [
-    { title: "Some title" },
-    { title: "Another title <b>With Html</b>" }
-  ];
+  public rows: Array<any> = [];
 
   constructor(public changeDetector: ChangeDetectorRef) {
+  }
+
+  public ngOnInit() {
+    let restProvider = SimpleRestProvider('http://localhost:3000');
+    
+    let results = restProvider(GET_LIST, this.objects, {
+      pagination: { page: 1, perPage: 10 },
+      sort: { field: 'id', order: 'ASC' }
+    }).then((results) => {
+      this.rows = results.data;
+    });
+
+    console.log(results);
   }
 
   public ngAfterContentInit() {
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,2 +1,23 @@
 // App
 export * from './app.module';
+
+import FakeRest from 'fakerest';
+import fetchMock from 'fetch-mock';
+
+const restServer = new FakeRest.FetchServer('http://localhost:3000');
+restServer.init({
+    'authors': [
+        { id: 0, first_name: 'Leo', last_name: 'Tolstoi' },
+        { id: 1, first_name: 'Jane', last_name: 'Austen' }
+    ],
+    'books': [
+        { id: 0, author_id: 0, title: 'Anna Karenina' },
+        { id: 1, author_id: 0, title: 'War and Peace' },
+        { id: 2, author_id: 1, title: 'Pride and Prejudice' },
+        { id: 3, author_id: 1, title: 'Sense and Sensibility' }
+    ]
+});
+
+restServer.toggleLogging(); // logging is off by default, enable it
+fetchMock.mock('^http://localhost:3000', restServer.getHandler());
+    //return () => fetchMock.restore();
